4c3386eb3ea82d03c441aac93d4d81d0c561fa38
pjpawel <pawel.jan.podgorski@gmail.com>
2023-06-09 21:46:29 +0200

    Add config serailizing

--- src/Component/Config/ConfigLoader.php
+++ src/Component/Config/ConfigLoader.php
@@ -7,6 +7,8 @@ use LiteApi\Component\Config\Wrapper\ConfigWrapper;
 class ConfigLoader
 {
 
+    private const CACHE_SERIALIZED_PATH = '/var/cache/config';
+
     public function __construct(
         private readonly string $projectDir
     )
@@ -15,7 +17,7 @@ class ConfigLoader
 
     public function getConfig(): ConfigWrapper
     {
-        $serializedConfigPath = $this->projectDir . '/var/cache/config';
+        $serializedConfigPath = $this->projectDir . self::CACHE_SERIALIZED_PATH;
         if (file_exists($serializedConfigPath)) {
             $lastModified = filemtime($serializedConfigPath);
             if ($lastModified !== false && $lastModified + 360 > time()) {
@@ -25,7 +27,19 @@ class ConfigLoader
         $env = new Env();
         $envWrapper = $env->getEnvParams($this->projectDir);
         $configArray = require $this->projectDir . '/config/config.php';
-        return new ConfigWrapper($envWrapper, $configArray);
+        $config = new ConfigWrapper($envWrapper, $configArray);
+        if (!$config->envParams->debug) {
+            $this->serializeConfig($config);
+        }
+        return $config;
+    }
+
+    private function serializeConfig(ConfigWrapper $config): void
+    {
+        file_put_contents(
+            $this->projectDir . self::CACHE_SERIALIZED_PATH,
+            serialize($config)
+        );
     }
 
 }
