527278842d5702cec03d7dc6fde71a7ab8d0e535
pjpawel <pawel.jan.podgorski@gmail.com>
2023-06-13 18:39:13 +0200

    Use setters

--- src/Component/Common/ObjectWrapper.php
+++ src/Component/Common/ObjectWrapper.php
@@ -2,6 +2,8 @@
 
 namespace LiteApi\Component\Common;
 
+use Exception;
+use LiteApi\Component\Config\Wrapper\Setter;
 use LiteApi\Exception\ProgrammerException;
 use ReflectionClass;
 
@@ -10,7 +12,7 @@ class ObjectWrapper
 
     /**
      * Method to create objects from array:
-     * Array should have key 'class' and optional 'args'
+     * Array should have key 'class' and optional 'args' and 'setters'
      *
      * @param array $config
      * @return mixed
@@ -22,11 +24,27 @@ class ObjectWrapper
         if (!isset($config['class'])) {
             throw new ProgrammerException('Cannot create class from config');
         }
+        $reflectionClass = new ReflectionClass($config['class']);
         if (isset($config['args'])) {
-            return (new ReflectionClass($config['class']))->newInstanceArgs($config['args']);
+            $object = $reflectionClass->newInstanceArgs($config['args']);
         } else {
-            return new $config['class']();
+            $object = new $config['class']();
         }
+        if (isset($config['setters'])) {
+            $setters = $config['setters'];
+            if (!is_array($setters)) {
+                throw new ProgrammerException();
+            }
+            foreach ($setters as $setter) {
+                try {
+                    $setterWrapper = new Setter($setter);
+                    $reflectionClass->getMethod($setterWrapper->method)->invoke($object, $setterWrapper->args);
+                } catch (Exception $e) {
+                    throw new ProgrammerException('Cannot use setter: ' . var_export($setter, true), 0, $e);
+                }
+            }
+        }
+        return $object;
     }
 
 }
